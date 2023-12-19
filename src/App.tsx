c708b3092a88b7160c72b21b48661d648a4b6c57
Vladimir <vladimir@Air-Vladimir.Dlink>
2023-12-19 17:00:33 +0300

    add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div>APP</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque at consequatur architecto molestiae, tenetur ex? Aliquam atque placeat
+      distinctio animi architecto officiis commodi repellat? Facere quibusdam nam possimus ipsam error? Стильный электросамокат с мощным
+      аккумулятором
+    </div>
+  );
 }
 
 export default App;
